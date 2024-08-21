a0aeb4cbf17318fbc266fb7eaa53ec62cbd26b58
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2024-08-21 08:23:48 +0330

    Type Inference

---
+++ variables5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Type Inference
+// C# allows type inference using the var keyword, where the compiler determines the type based on the assigned value.
+
+var cityName = "New York";  
+var number = 42;  
+
+Console.WriteLine(cityName);
+Console.WriteLine(number);
