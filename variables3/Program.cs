5a1aaaae6cf7e7451881e15f08d72bb923070852
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2024-08-21 08:05:07 +0330

    Nullable Types

---
+++ variables3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Nullable Types
+// Nullable types are used when a value type needs to represent the absence of a value (null).
+
+// "int?": A nullable integer type, which can hold any valid int value or null.
+int? nullableInt = null;
+Console.WriteLine(nullableInt);
+// ---------------------------------------------------------------------------
