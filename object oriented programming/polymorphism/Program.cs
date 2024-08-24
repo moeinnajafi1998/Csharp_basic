a7555ff157ed5ea1662fc7862749f530b4f958b7
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2024-08-24 17:54:30 +0330

    polymorphism

---
+++ object oriented programming/polymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿// Polymorphism:
+// olymorphism allows objects to be treated as instances of their parent class rather than their actual class.
+// It can be achieved through method overriding and interfaces.
+// This enables you to write more flexible and extensible code.
+public class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("Animal sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+
+class Program{
+    static void Main(string[] args){
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+        myDog.Speak(); // Output: Woof!
+        myCat.Speak(); // Output: Meow!
+        
+    }
+}
