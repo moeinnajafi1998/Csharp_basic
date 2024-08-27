817530e723485149e6633b0e54adbcda1270eb8a
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2024-08-27 10:49:17 +0330

    method overloading

---
+++ methodoverloading/Program.cs
@@ -0,0 +1,21 @@
+﻿// With method overloading, multiple methods can have the same name with different parameters(signature):
+
+
+
+class Program{
+    static int sampleMethod(int x)
+    {
+        return x;
+    }
+    static int sampleMethod(int x, int y)
+    {
+        return x + y;
+    }
+    static void Main(string[] args)
+    {
+        int myNum1 = sampleMethod(8);
+        int myNum2 = sampleMethod(8,7);
+        Console.WriteLine("Int: " + myNum1);
+        Console.WriteLine("Int: " + myNum2);
+    }
+}
